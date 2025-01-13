e1b9de027507aba0b1295d8cae8018f459395663
angelvelez0733 <167249926+angelvelez0733@users.noreply.github.com>
2025-01-13 17:04:09 -0500

    validators from register fucntionality OK

--- src/routes/users/user.ts
+++ src/routes/users/user.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import registerController from "../../controllers/users/registerController";
+import { validationParamsRegister, validationRegister } from "../../middlewares/validators/validatorRegister";
 const router = express.Router();
 
 
-router.post("/", registerController);
+router.post("/", validationParamsRegister, validationRegister, registerController);
 
 export default router;
